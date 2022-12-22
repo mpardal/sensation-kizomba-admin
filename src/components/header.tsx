f93ab229a0744604a5066ae189d647e6e0514083
Mickael <mickael.pardal@hotmail.fr>
2022-12-22 11:00:34 +0100

    chore: remove useless pr

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <Box as="header" w="full" h="4.5rem" pos="sticky" top={0} bg="chakra-body-bg" zIndex={10}>
       <HStack spacing={6} maxW="8xl" mx="auto" w="full" h="full" px={6}>
-        <Flex justifyContent="space-between" w="full" pr={6} as="section">
+        <Flex justifyContent="space-between" w="full" as="section">
           <Heading as="h1" size="lg" className="font-cagliostro" color="orange.200">
             Sensation Kizomba
           </Heading>
